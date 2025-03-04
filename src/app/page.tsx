7b3eda7ab40767ce0c68c3776868ffa2eb1bdea4
COSSON <153493631+cosinussnus@users.noreply.github.com>
2025-03-04 02:36:00 +0100

    creation of the follow sys

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+//import { Button } from "@/components/ui/button";
+import CreatePost from "@/components/CreatePost";
+import WhoToFollow from "@/components/WhoToFollow";
+//import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
+  const user = await currentUser();
   return (
-    <div className="m-4">
-      <h1>home page content</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
+
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <WhoToFollow />
+      </div>
     </div>
   );
   }
